0d7993e04f1d63e312c225a2de408bdcf3a61bde
TTTruong <tttruong@vnpt.vn>
2020-02-10 16:32:46 +0700

    first

---
+++ src/pages/Index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="starter-page">
+        <div class="section text-center pb-0">
+            <h3>Mã hóa MD5</h3>
+        </div>
+        <md5></md5>
+    </div>
+</template>
+<script>
+    import md5 from './components/md5';
+    export default {
+        name: 'index',
+        components :{
+            md5
+        }
+    };
+</script>
+<style>
+    .starter-page {
+        min-height: calc(100vh - 100px);
+    }
+</style>
---
+++ src/pages/components/md5.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="container">
+            <card class="card-nav-tabs text-center" header-classes="card-header-warning">
+                <div slot="header" class="mt-2">
+                    Mã hóa MD5
+                </div>
+                <div class="form-group">
+                    <label for="">String</label>
+                    <textarea class="form-control border" name="" id="" rows="10" v-model="inputArea"></textarea>
+                </div>
+                <p>{{inputArea}}</p>
+                <n-button type="primary" v-on:click="convert">Mã hóa</n-button>
+            </card>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import {Card} from '@/components'
+    import {Button} from '@/components'
+
+    export default {
+        name: "md5.vue",
+        components: {
+            Card,
+            "n-button": Button,
+        },
+        data() {
+            inputArea : ''
+        },
+        methods : {
+            convert : function () {
+                console.log(inputArea)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    textarea.form-control {
+        max-height: 500px;
+    }
+</style>
